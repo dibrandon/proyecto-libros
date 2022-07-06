7d386f6484b2a01acc1760508da511a8f1864302
alejandro_diaz <diazbrandonalejandro@gmail.com>
2022-07-06 12:41:26 +0200

    nuevo seed a db, cards de explore funcionando

--- seeds/books.seed.js
+++ seeds/books.seed.js
@@ -1,7 +1,7 @@
 const booksArr = [
-    {name: "los gatos de ulthar", author: "lovecraft", idiom: "español", url: "https://drive.google.com/file/d/1FC-Q2_ut1L2B18yBPLBmWKeVE9mwlfyX/view?usp=sharing"},
-    {name: "la llamada de cthulhu", author: "lovecraft", idiom: "español", url: "https://drive.google.com/file/d/1gdYGFSAu96-qaS39JtU7T_V0YWE2ZKq-/view?usp=sharing"},
-    {name: "las ratas de las paredes", author: "lovecraft", idiom: "español", url: "https://drive.google.com/file/d/1j3t3Z3MoJjS70mPIyfwtOXmU5ijflaf4/view?usp=sharing"}
+    {name: "los gatos de ulthar", author: "lovecraft", idiom: "español", url: "https://drive.google.com/file/d/1FC-Q2_ut1L2B18yBPLBmWKeVE9mwlfyX/view?usp=sharing", imgUrl: "https://i.postimg.cc/zGspZPHZ/Captura-de-Pantalla-2022-07-06-a-las-12-12-29.png"},
+    {name: "la llamada de cthulhu", author: "lovecraft", idiom: "español", url: "https://drive.google.com/file/d/1gdYGFSAu96-qaS39JtU7T_V0YWE2ZKq-/view?usp=sharing", imgUrl: "https://i.postimg.cc/Kj38Dn3r/Weird-Tales-volume-11-number-02-page-159-The-Call-of-Cthulu-uncaptioned.jpg"},
+    {name: "las ratas de las paredes", author: "lovecraft", idiom: "español", url: "https://drive.google.com/file/d/1j3t3Z3MoJjS70mPIyfwtOXmU5ijflaf4/view?usp=sharing", imgUrl: "https://i.postimg.cc/gjm06dwy/Captura-de-Pantalla-2022-07-06-a-las-12-01-59.png"}
 ]
 
 // aqui insertaremos la data a la BD
@@ -28,4 +28,4 @@ mongoose
     console.log(err);
   });
 
-  
+
--- seeds/recommended.seed.js
+++ seeds/recommended.seed.js
@@ -1,13 +1,13 @@
-const booksArr = [
-    {name: "los gatos de ulthar", author: "lovecraft", idiom: "español", url: "https://drive.google.com/file/d/1FC-Q2_ut1L2B18yBPLBmWKeVE9mwlfyX/view?usp=sharing"},
-    {name: "la llamada de cthulhu", author: "lovecraft", idiom: "español", url: "https://drive.google.com/file/d/1gdYGFSAu96-qaS39JtU7T_V0YWE2ZKq-/view?usp=sharing"},
-    {name: "las ratas de las paredes", author: "lovecraft", idiom: "español", url: "https://drive.google.com/file/d/1j3t3Z3MoJjS70mPIyfwtOXmU5ijflaf4/view?usp=sharing"}
+const recoArr = [
+    {name: "los gatos de ulthar", author: "lovecraft", idiom: "español", url: "https://drive.google.com/file/d/1FC-Q2_ut1L2B18yBPLBmWKeVE9mwlfyX/view?usp=sharing", imgUrl: "https://i.postimg.cc/zGspZPHZ/Captura-de-Pantalla-2022-07-06-a-las-12-12-29.png"},
+    {name: "la llamada de cthulhu", author: "lovecraft", idiom: "español", url: "https://drive.google.com/file/d/1gdYGFSAu96-qaS39JtU7T_V0YWE2ZKq-/view?usp=sharing", imgUrl: "https://i.postimg.cc/Kj38Dn3r/Weird-Tales-volume-11-number-02-page-159-The-Call-of-Cthulu-uncaptioned.jpg"},
+    {name: "las ratas de las paredes", author: "lovecraft", idiom: "español", url: "https://drive.google.com/file/d/1j3t3Z3MoJjS70mPIyfwtOXmU5ijflaf4/view?usp=sharing", imgUrl: "https://i.postimg.cc/gjm06dwy/Captura-de-Pantalla-2022-07-06-a-las-12-01-59.png"}
 ]
 
 // aqui insertaremos la data a la BD
 // los seeds no se conectan con nuestro servidor
 
-const Book = require("../models/Book.model");
+const Reco = require("../models/recommended.model");
 
 // insertarla en la BD
 const mongoose = require("mongoose");
@@ -15,12 +15,12 @@ const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://localhost:27017/freebooks-app")
   .then((response) => {
-    console.log("conectados a la Dase de Datos");
+    console.log("DB arriba");
 
-    return Book.insertMany(booksArr);
+    return Reco.insertMany(recoArr);
   })
   .then((response) => {
-    console.log("Books agregados correctamente");
+    console.log("Recomendados agregados correctamente");
     // hacer la desconeccion
     mongoose.connection.close();
   })
